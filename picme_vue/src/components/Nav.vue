23b5b34827b4e7103f91cda40ef2cee23daea279
gmenke54 <menke.gw@gmail.com>
2022-02-03 11:11:42 -0800

    added category form page

--- picme_vue/src/components/Nav.vue
+++ picme_vue/src/components/Nav.vue
@@ -3,6 +3,7 @@
     <!-- Nav Links Go Here -->
         <router-link to="/" name="Home">Home</router-link>
     <router-link to="/categories">Categories</router-link>
+    <router-link to="/categoryform">Add a Category</router-link>
   </nav>
 </template>
 
