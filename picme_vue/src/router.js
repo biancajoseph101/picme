ec55c26b0bfb60b2c01e4537fdfa5242467de830
andora814 <smith.doran@gmail.com>
2022-02-03 14:14:18 -0500

    matching up

--- picme_vue/src/router.js
+++ picme_vue/src/router.js
@@ -1,11 +1,10 @@
-import VueRouter from 'vue-router'
-import HomePage from './Pages/HomePage'
-import Category from './Pages/Category'
+import VueRouter from 'vue-router';
+import HomePage from './Pages/HomePage';
+import Category from './Pages/Category';
 
 const routes = [
-  {path: '/', component: HomePage, name: 'HomePage'},
-  {path: '/categories', component: Category, name: 'Category'},
-  
-]
+  { path: '/', component: HomePage, name: 'HomePage' },
+  { path: '/categories', component: Category, name: 'Category' }
+];
 
-export default new VueRouter ({routes, mode: 'history'})
+export default new VueRouter({ routes, mode: 'history' });
