a91c101f572c0171a4e7f6efed7e64a023a1c1e1
Joseph Venable <JJVenable@gmail.com>
2022-02-03 11:51:00 -0500

    Front end vue app created, nothing inside

---
+++ picme_vue/src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>This is home</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent'
+}
+</script>
+
+
+<style>
+    h3 {
+        color: #80cbc4;
+    }
+</style>
