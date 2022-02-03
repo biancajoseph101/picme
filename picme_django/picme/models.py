ef19b99d2aa640867bc77544240f3b9b6a254bc6
biancajoseph101 <biancajoseph101@gmail.com>
2022-02-03 11:53:27 -0600

    rest api

--- picme_django/picme/models.py
+++ picme_django/picme/models.py
@@ -2,18 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     img_url = models.TextField()
 
     def __str__(self):
-      return self.name
+        return self.name
+
 
 class Image(models.Model):
-    category = models.ManyToManyField(Category)
+    category = models.ManyToManyField(
+        Category, related_name="image_list", blank=True)
     title = models.CharField(max_length=50, default='no image title')
     img = models.FileField()
 
     def __str__(self):
-      return self.title
+        return self.title
